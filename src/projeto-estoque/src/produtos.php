6a170bd58cf6e659b8f39d485f8b916a8a5b1025
nely de faria <nelly.nf54@gmail.com>
2022-12-24 10:45:09 -0300

    Create produtos.php

---
+++ src/projeto-estoque/src/produtos.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'codigo' => '0001',
+        'nome' => 'Produto Teste 1',
+        'preco' => 'R$ 139,90',
+    ],
+    [
+        'codigo' => '0002',
+        'nome' => 'Produto Teste 2',
+        'preco' => 'R$ 339,90',
+    ],
+    [
+        'codigo' => '0003',
+        'nome' => 'Produto Teste 3',
+        'preco' => 'R$ 199,90',
+    ],
+    [
+        'codigo' => '0004',
+        'nome' => 'Produto Teste 4',
+        'preco' => 'R$ 189,99',
+    ]
+];
