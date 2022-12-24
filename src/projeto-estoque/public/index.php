f98a1d17a39e56b36c8ba431b8f6886b8583fc05
nely de faria <nelly.nf54@gmail.com>
2022-12-24 10:44:43 -0300

    Create index.php

---
+++ src/projeto-estoque/public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require  __DIR__ . '/../bootstrap.php';
+
+$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : '/';
+
+if ($pagina == '/') {
+    $title = 'Listagem de Produtos';
+    require  TEMPLATES . '/lista.php';
+}
+
+if ($pagina == '/produto') {
+
+    $codigoProduto = isset($_GET['codigo']) ? $_GET['codigo'] : '';
+
+    if ($codigoProduto) {
+        $produtoDetalhe = '';
+        // foreach (PRODUTOS as $produto) {
+        //     if ($produto['codigo'] == $codigoProduto) {
+        //         $produtoDetalhe = $produto;
+        //         break;
+        //     }
+        // }
+
+        $produtoDetalhe = array_filter(PRODUTOS, function($produto) use($codigoProduto) {
+            return $produto['codigo'] == $codigoProduto;
+        });
+
+        // var_dump($produtoDetalhe); exit;
+        $produtoDetalhe = current($produtoDetalhe);
+    }
+
+    if (!$codigoProduto || !isset($produtoDetalhe) || !$produtoDetalhe) exit('Produto não existe');
+
+    $title = 'Detalhes do Produto';
+    require TEMPLATES . '/produto.php';
+}
