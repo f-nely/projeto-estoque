53c7dc2bb14ce624c443ed42f93759beb2343f00
nely de faria <nelly.nf54@gmail.com>
2022-12-24 10:45:56 -0300

    Create produto.php

---
+++ src/projeto-estoque/templates/produto.php
@@ -0,0 +1,5 @@
+<?php include TEMPLATES . '/includes/header.php' ?>
+    <?php echo $produtoDetalhe['nome'] ?>
+    <hr>
+    <?php echo $produtoDetalhe['preco'] ?>
+<?php include TEMPLATES . '/includes/footer.php' ?>
