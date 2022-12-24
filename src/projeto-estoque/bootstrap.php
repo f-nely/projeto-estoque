744b07c88741677bb5f3851be8abc7131e1d2ef0
nely de faria <nelly.nf54@gmail.com>
2022-12-24 10:44:22 -0300

    Create bootstrap.php

---
+++ src/projeto-estoque/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+// Configuração projeto
+define('HOME', '/projeto-estoque/public/');
+define('PROJETO', 'Code Estoque');
+
+define('TEMPLATES', __DIR__ . '/templates');
+
+// Arrays com os produtos
+define('PRODUTOS', require __DIR__ . '/src/produtos.php');
+
