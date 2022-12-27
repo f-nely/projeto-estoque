425b6e731fb6a7514857f9664d1dd266852277f3
nely de faria <nelly.nf54@gmail.com>
2022-12-27 17:54:49 -0300

    Create criar-produto.php

---
+++ src/projeto-estoque/templates/criar-produto.php
@@ -0,0 +1,10 @@
+<?php include TEMPLATES . '/includes/header.php' ?>
+
+<h1>Criar Produto</h1>
+
+<form action="">
+    <label for="name">Full Name:</label>
+    <input type="text" name="name" id="name" placeholder="Your complete name">
+</form>
+
+<?php include TEMPLATES . '/includes/footer.php' ?>
